5881c0fb5f287f50a36629903ed1856817ec42a5
Wahidullahsharifi <fausharifi@gmail.com>
2024-07-09 18:15:56 -0700

    lets Starts 2

---
+++ src/test/java/tak/intro/HandleBrowser.java
@@ -0,0 +1,19 @@
+package tak.intro;
+
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class HandleBrowser {
+
+    public static void main(String[] args) throws InterruptedException {
+
+        ChromeDriver driver = new ChromeDriver();
+        driver.get("https://facebook.com");
+        driver.manage().window().maximize();
+        Thread.sleep(5000);
+
+        driver.close();
+
+
+    }
+
+}
